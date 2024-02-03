5657fad60877f5a8c3b26dec85b0643ead2ade70
Antonio Herrera <artadapt@gmail.com>
2024-02-03 16:14:37 +0100

    Primer commit, header

--- tienda/src/app/app-routing.module.ts
+++ tienda/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    //canActivate: [AuthGuard],
+    children: [
+      { path: '', component: DashboardComponent,data:{title:'Dashboard'} },
+      
+
+    ],
+  },
+  { path: '', redirectTo: "/dashboard", pathMatch: 'full'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
